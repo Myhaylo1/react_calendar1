2f8d1d61b34ba7dc353e167c4eec5f6d803bdddd
Myhaylo1 <zpkazka@gmail.com>
2020-08-12 08:37:41 +0300

    Learn props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import Stopwatch from './components/Stopwatch';
 import Calendar from './components/Calendar';
 
--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -3,19 +3,30 @@ import styles from './Calendar.module.scss';
 //import * as dateFns from 'date-fns';
 
 let currentDate=new Date();
+//let currentDate=new Date(2020,9-1,11);
 let currentDateWeekDay=currentDate.toLocaleString('en-US',{weekday:'long'})
-let currentDateDay=currentDate.toLocaleString('en-US',{day: 'numeric'})
-let currentDateMonth=currentDate.toLocaleString('en-US',{month: 'long'})
-let currentDateYear=currentDate.toLocaleString('en-US',{year: 'numeric'})
+let currentDateDayOfWeek=currentDate.getDay();
+let currentDateDay=Number(currentDate.toLocaleString('en-US',{day: 'numeric'}));
+// let currentDateMonth=currentDate.toLocaleString('en-US',{month: 'long'});
+let currentDateMonth=currentDate.getMonth();
+let currentDateYear=Number(currentDate.toLocaleString('en-US',{year: 'numeric'}));
 let currentDateMonthYear=currentDate.toLocaleString('en-US',{month: 'long', year: 'numeric'})
+let dayInMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
 let cellValue=[];
 
 function fillCellValue()
 {
+  dayInMonth[1]=28;
+  if (!(currentDateYear % 4)) dayInMonth[1]++;
   cellValue=[];
   cellValue.push('S','M','T','W','T','F','S');
-  for (let i=7;i<49;i++) cellValue.push(i);
-  console.log(cellValue);
+  for (let i=7;i<49;i++) cellValue.push('');
+  let firstWeekDay=currentDateDay % 7;
+  if (currentDateDayOfWeek<firstWeekDay-1) firstWeekDay=currentDateDayOfWeek+8-firstWeekDay; else firstWeekDay=Math.abs(firstWeekDay-currentDateDayOfWeek-1);
+//  for (let i=7;i<7+firstWeekDay;i++) cellValue[i]='';
+  let j=1;
+  for (let i=firstWeekDay+7;i<firstWeekDay+7+dayInMonth[currentDateMonth];i++) cellValue[i]=j++;  
+  console.log(firstWeekDay,dayInMonth[currentDateMonth],currentDateDayOfWeek,' ',currentDateWeekDay,cellValue);
   return cellValue;
 }
 
@@ -61,17 +72,12 @@ cellValue=fillCellValue();
 //   }
 // }
 
-class Hello extends React.Component {
-  render() {
-    return <div className={this.props.className}>Привет, {this.props.toWhat}!</div>;
-  }
-}
-
 class CalendarSheetElement extends React.Component {
   render() {
     const elements = [];
     for (let i=0; i<49; ++i) {
-      elements.push(<a className={styles.calendarTableCell}>{cellValue[i]}</a>);
+//      elements.push(<a className={styles.calendarTableCell}>{cellValue[i]}</a>);
+      elements.push(<a key={i} className={styles.calendarTableCell} style={{borderColor: currentDateDay!==cellValue[i]?'white':'red'}}>{cellValue[i]}</a>);
     }
     if (!this.props.sheet)
       return (
@@ -89,6 +95,7 @@ class CalendarSheetElement extends React.Component {
 
 class CalendarSheet extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <article className={styles.calendarSheet}>
         <h3 style={this.props.style}>{this.props.text}</h3>
